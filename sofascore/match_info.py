a986635fd6fbd66ea9763fc365092cd1c3bd8fa1
Pavel Shyliahau <pashxa@syntronic.com>
2023-11-21 11:44:42 -0500

    Use rapid api

--- sofascore/match_info.py
+++ sofascore/match_info.py
@@ -5,18 +5,18 @@ from utils.datetime import timestampToDate
 
 _headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+    'X-RapidAPI-Key': '6wLYeGGlGhmshLygKOkr7Gioib2Fp1rW6z3jsnbtknqGtW8nuv'
 }
 
-_base_crawler_url = 'https://api.crawlbase.com/?token=RA4zal8GzJuqs_0759kP1A&url='
-_event_url_template = "https://api.sofascore.com/api/v1/event/{}"
-_lineups_path = "/lineups"
-_standings_url_format = "https://api.sofascore.com/api/v1/tournament/{}/season/{}/standings/total"
-_team_form_url_format = "https://api.sofascore.com/api/v1/team/{}/events/last/0"
-_h2h_events_url_format = "https://api.sofascore.com/api/v1/event/{}/h2h/events"
+_event_url_template = "https://sofascore.p.rapidapi.com/matches/detail?matchId={}"
+_lineups_url_template = "https://sofascore.p.rapidapi.com/matches/get-lineups?matchId={}"
+_standings_url_format = "https://sofascore.p.rapidapi.com/tournaments/get-standings?tournamentId={}&seasonId={}"
+_team_form_url_format = "https://sofascore.p.rapidapi.com/teams/get-last-matches?teamId={}&page=0"
+_h2h_events_url_format = "https://sofascore.p.rapidapi.com/matches/get-head2head?matchId={}"
 
 
 def fetchTeamsAndDate(event_id):
-   web_url = _base_crawler_url + _event_url_template.format(event_id)
+   web_url = _event_url_template.format(event_id)
    response = requests.get(web_url, headers=_headers)
    print(response)      
    json = response.json()
@@ -26,10 +26,9 @@ def fetchTeamsAndDate(event_id):
       'home': event['homeTeam'],
       'away': event['awayTeam'],
       'season': {
-         'tournament': event['tournament']['id'],
+         'tournament': event['tournament']['uniqueTournament']['id'],
          'id': event['season']['id']
-      },
-      'h2h_event_id': event['customId']
+      }
    }
 
 def parseLineups(team):
@@ -45,7 +44,7 @@ def parseLineups(team):
   }
 
 def fetchLineups(event_id):
-   web_url = _base_crawler_url +  _event_url_template.format(event_id) + _lineups_path
+   web_url = _lineups_url_template.format(event_id)
    response = requests.get(web_url, headers=_headers)
    if response.status_code != 200:
       return {
@@ -53,13 +52,22 @@ def fetchLineups(event_id):
          'away': 'Not available'
       }
    json = response.json()
+
+   homeLineup = 'Not available'
+   if 'home' in json:
+      homeLineup = parseLineups(json['home'])
+   
+   awayLineup = 'Not available'
+   if 'away' in json:
+      awayLineup = parseLineups(json['away'])
+
    return {
-      'home': parseLineups(json['home']),
-      'away': parseLineups(json['away'])
+      'home': homeLineup,
+      'away': awayLineup
    }
 
 def fetchTableStandings(season):
-  web_url = _base_crawler_url +  _standings_url_format.format(season['tournament'], season['id'])
+  web_url = _standings_url_format.format(season['tournament'], season['id'])
   response = requests.get(web_url, headers=_headers)
   json = response.json()
   # Extract the relevant data
@@ -67,7 +75,7 @@ def fetchTableStandings(season):
   return standings
 
 def fetchForm(team):
-   web_url = _base_crawler_url +  _team_form_url_format.format(team['id'])
+   web_url = _team_form_url_format.format(team['id'])
    response = requests.get(web_url, headers=_headers)
    json = response.json()
    filtered_sorted_matches = sorted(
@@ -76,27 +84,36 @@ def fetchForm(team):
    
    return "; ".join(f"{timestampToDate(event['startTimestamp'])}: {event['homeTeam']['name']} {event['homeScore']['current']}-{event['awayScore']['current']} {event['awayTeam']['name']}" for event in filtered_sorted_matches)
 
-def fetchH2HResults(custom_id):
-   web_url = _base_crawler_url + _h2h_events_url_format.format(custom_id)
+def fetchH2HResults(matchId):
+   web_url = _h2h_events_url_format.format(matchId)
    response = requests.get(web_url, headers=_headers)
    json = response.json()
+   events = [event for tournament in json["tournaments"] for event in tournament["events"]]
    filtered_sorted_matches = sorted(
-    [item for item in json['events'] if item['status']['type'] == "finished"],
+    [item for item in events if item['status']['type'] == "finished"],
     key=lambda x: x['startTimestamp'])
    
    return "; ".join(f"{timestampToDate(event['startTimestamp'])}: {event['homeTeam']['name']} {event['homeScore']['current']}-{event['awayScore']['current']} {event['awayTeam']['name']}" for event in filtered_sorted_matches)
 
 def getMatchInfo(event_id):
+  print('Fetching match info')
   matchInfo = fetchTeamsAndDate(event_id)
+  print("Success")
   homeTeam = matchInfo['home']
   awayTeam = matchInfo['away']
+  print('Fetching match info')
   lineups = fetchLineups(event_id)
+  print("Success")
   homeTeam['lineup'] = lineups['home']
   awayTeam['lineup'] = lineups['away']
   homeTeam['form'] = fetchForm(homeTeam)
   awayTeam['form'] = fetchForm(awayTeam)
+  print('Fetching Standings')
   standings = fetchTableStandings(matchInfo['season'])
-  h2hResults = fetchH2HResults(matchInfo['h2h_event_id'])
+  print("Success")
+  print('Fetching H2H')
+  h2hResults = fetchH2HResults(event_id)
+  print("Success")
   event_name = f"{matchInfo['date']}: {homeTeam['name']} vs {awayTeam['name']}"
 
   return {
