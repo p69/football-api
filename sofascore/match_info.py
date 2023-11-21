bfc35590a1faecffae3eef7323867dd49ba90571
Pavel Shyliahau <pashxa@syntronic.com>
2023-11-21 17:45:39 -0500

    Remove api key print statement

--- sofascore/match_info.py
+++ sofascore/match_info.py
@@ -5,7 +5,6 @@ import os
 from utils.datetime import timestampToDate
 
 rapid_api_key = os.getenv('RAPIDAPI_KEY')
-print(f"key = {rapid_api_key}")
 _headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
     'X-RapidAPI-Key': rapid_api_key
