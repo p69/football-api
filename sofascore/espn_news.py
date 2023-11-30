fd063dc510ee8fe6a7a84fa93f2895c82c328dc0
Pavel Shyliahau <pashxa@syntronic.com>
2023-11-30 10:22:15 -0500

    Add more leagues

--- sofascore/espn_news.py
+++ sofascore/espn_news.py
@@ -7,6 +7,9 @@ _espn_news_for_league_url_template = "http://site.api.espn.com/apis/site/v2/spor
 
 def getLatestESPNNews(league: FootballLeague):
   print(f"Fetching ESPN news for {league}")
+  espn_slug = league.get_espn_slug()
+  if espn_slug == None:
+    return f"No news sources for league {league}"
   web_url = _espn_news_for_league_url_template.format(league.get_espn_slug())
   response = requests.get(web_url, headers=headers)
   json = response.json()
--- sofascore/leagues.py
+++ sofascore/leagues.py
@@ -8,6 +8,18 @@ class FootballLeague(Enum):
   FRANCE=(34, 52571)
   CHAMPIONS_LEAGUE=(7, 52162)
   EUROPA_LEAGUE=(679, 53654)
+  DUTCH_EREDIVISIE=(37, 52554)
+  AUSTRIA=(45, 52524)
+  SWITZERLAND=(215, 52366)
+  BELGIUM=(38, 52383)
+  DENMARK=(39, 52172)
+  NORWAY=(20, 47806)
+  POLAND=(202, 52176)
+  IRELAND=(192, 47946)
+  ROMANIA=(152, 52541)
+  SCOTLAND=(36, 52588)
+  SWEDEN=(40, 47730)
+  TURKEY=(52, 53190)
 
   def __init__(self, id, latestSeason):
     self.id = id
@@ -21,9 +33,13 @@ class FootballLeague(Enum):
       return None
     
   def get_livescore_slug(self):
+    if self not in _league_to_livescore_slug_map:
+      return None
     return _league_to_livescore_slug_map[self]
   
   def get_espn_slug(self):
+    if self not in _league_to_espn_slug_map:
+      return None
     return _league_to_espn_slug_map[self]
 
 _league_to_livescore_slug_map = {
@@ -44,4 +60,15 @@ _league_to_espn_slug_map = {
   FootballLeague.FRANCE: "fra.1",
   FootballLeague.CHAMPIONS_LEAGUE: "uefa.champions",
   FootballLeague.EUROPA_LEAGUE: "uefa.europa",
+  FootballLeague.DUTCH_EREDIVISIE: "ned.1",
+  FootballLeague.AUSTRIA: "aut.1",
+  FootballLeague.SWITZERLAND: "sui.1",
+  FootballLeague.BELGIUM: "bel.1",
+  FootballLeague.DENMARK: "den.1",
+  FootballLeague.NORWAY: "nor.1",
+  FootballLeague.IRELAND: "irl.1",
+  FootballLeague.ROMANIA: "rou.1",
+  FootballLeague.SCOTLAND: "sco.1",
+  FootballLeague.SWEDEN: "swe.1",
+  FootballLeague.TURKEY: "tur.1",
 }
